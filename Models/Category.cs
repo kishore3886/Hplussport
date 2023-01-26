cdf268e1e2d93ab14cad6dc64cf995ea8a167e60
Kishore kumar <kishore3886@gmail.com>
2023-01-26 14:39:18 -0800

    First commit

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Hplussport.API.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual List<Product> Products { get; set; }
+    }
+}
