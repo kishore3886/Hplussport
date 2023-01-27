08f1c9b845a12f6dcc452f4b26b4801b1d182451
Kishore kumar <kishore3886@gmail.com>
2023-01-26 16:53:00 -0800

    added code to get all products, one product and async methods to code.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Hplussport.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 namespace Hplussport.API.Controllers
 {
@@ -19,10 +20,30 @@ namespace Hplussport.API.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Product> GetAllProducts() {
+        //public IEnumerable<Product> GetAllProducts() {
 
-            return _shopContext.Products.ToArray();
+        //    return _shopContext.Products.ToArray();
+        //}
+
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _shopContext.Products.ToListAsync();
+
+            return Ok(products);
         }
+
+        [HttpGet,Route("{id:int}")]
+        public async Task<IActionResult> GetProduct(int id) { 
+        
+            var product = await _shopContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(_shopContext.Products.Find(id));
+        } 
+
 
        // add ef core and seed the data
 
