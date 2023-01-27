a41c3fc5552c16dcb69b391f37d58fcd8060f9f7
Kishore kumar <kishore3886@gmail.com>
2023-01-26 16:09:38 -0800

    returned a list of products.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,16 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Hplussport.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Collections.Generic;
 namespace Hplussport.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts() {
 
-            return "Ok.";
+        private readonly ShopContext _shopContext;
+
+        public ProductsController(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+
+            _shopContext.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts() {
+
+            return _shopContext.Products.ToArray();
         }
 
        // add ef core and seed the data
